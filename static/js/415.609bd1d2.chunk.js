9a1a75332035e9699a8b2ad82f6878ae059b3026
DmitriyP911 <54802150+DmitriyP911@users.noreply.github.com>
2023-06-12 14:46:14 +0000

    Deploying to gh-pages from @ DmitriyP911/some_repo@a0988aa485ede76963b9dfe4ed2266402524d37d 🚀

---
+++ static/js/415.609bd1d2.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[415],{5415:function(e,n,t){t.r(n);var c,i,o=t(168),r=t(9202),h=t(184),l=r.Z.div(c||(c=(0,o.Z)(["\n  display: flex;\n  align-items: center;\n  justify-content: center;\n  min-height: calc(100vh - 50px);\n"]))),a=r.Z.h1(i||(i=(0,o.Z)(["\n  font-weight: 500;\n  font-size: 48px;\n  text-align: center;\n"])));n.default=function(){return(0,h.jsx)(l,{children:(0,h.jsx)(a,{children:"Welcome to my PhoneBook!"})})}}}]);
+//# sourceMappingURL=415.609bd1d2.chunk.js.map
