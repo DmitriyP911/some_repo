e264457818dfa2f07e482eb51613b2706e2bfee2
Dmytro Pysmennyi <dmytro_pysmennyi@epam.com>
2023-06-12 18:25:08 +0300

    test2

--- src/App.jsx
+++ src/App.jsx
@@ -7,11 +7,15 @@ import { authOperations } from 'redux/auth';
 import { getRefresh } from 'redux/Selectors';
 import PublicRoute from './PublicRoute';
 import PrivateRoute from './PrivateRoute';
+import Home from 'pages/Home';
+import Contacts from 'pages/Contacts';
+import Register from 'pages/Register';
+import Login from 'pages/Login';
 
-const Home = lazy(() => import('./pages/Home'));
-const Contacts = lazy(() => import('./pages/Contacts'));
-const Register = lazy(() => import('./pages/Register'));
-const Login = lazy(() => import('./pages/Login'));
+// const Home = lazy(() => import('./pages/Home'));
+// const Contacts = lazy(() => import('./pages/Contacts'));
+// const Register = lazy(() => import('./pages/Register'));
+// const Login = lazy(() => import('./pages/Login'));
 
 export function App() {
   const dispatch = useDispatch();
@@ -23,30 +27,28 @@ export function App() {
   return isRefresing ? (
     <h1>Refreshing user...</h1>
   ) : (
-    <Suspense fallback={null}>
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<Home />} />
-          <Route
-            path="register"
-            element={
-              <PublicRoute redirectTo="/contacts" component={<Register />} />
-            }
-          />
-          <Route
-            path="login"
-            element={
-              <PublicRoute redirectTo="/contacts" component={<Login />} />
-            }
-          />
-          <Route
-            path="contacts"
-            element={
-              <PrivateRoute redirectTo="/login" component={<Contacts />} />
-            }
-          />
-        </Route>
-      </Routes>
-    </Suspense>
+    // <Suspense fallback={null}>
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<Home />} />
+        <Route
+          path="register"
+          element={
+            <PublicRoute redirectTo="/contacts" component={<Register />} />
+          }
+        />
+        <Route
+          path="login"
+          element={<PublicRoute redirectTo="/contacts" component={<Login />} />}
+        />
+        <Route
+          path="contacts"
+          element={
+            <PrivateRoute redirectTo="/login" component={<Contacts />} />
+          }
+        />
+      </Route>
+    </Routes>
+    // </Suspense>
   );
 }
